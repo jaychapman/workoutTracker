7962c1e2434de6885ae6fe45de3c6795ce3c7aa8
Jay C <jaychap77@yahoo.com>
2020-12-03 10:38:19 -0500

    update

--- server.js
+++ server.js
@@ -14,15 +14,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/htmlRoutes.js"))
